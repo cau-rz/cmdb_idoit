4ce2b73257d16579d2166ed48405da2c0aac0233
Daniel Ehlers <sargon@toppoint.de>
2017-11-27 15:42:14 +0100

    Refactor value handling

---
+++ cmdb_idoit/category/conversion.py
@@ -0,0 +1,113 @@
+"""
+    This file is part of cmdb_idoit.
+
+    cmdb_idoit is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    cmdb_idoit is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with cmdb_idoit.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import datetime
+import logging
+
+def conver_list(func,values):
+    if values is None:
+        return []
+    else:
+        if isinstance(values,list):
+            return [ func(value) for value in values ]
+        else:
+            raise Exception("Expected value of type list, but instead it has type %s '%s'" % (type(values),repr(values)))
+
+def conver_integer(value):
+    if value is None:
+        return None
+    elif isinstance(value,str):
+        try:
+          return int(value)
+        except ValueError:
+            raise Exception("Can't convert %s to integer" % value,e)
+    else:
+        raise Exception("Conversion of none string to integer is not supported '%s'" % repr(value))
+
+def conver_string(value):
+    if value is None:
+        return None
+    elif isinstance(value,str):
+        return value
+    else:
+        raise Exception("Value is not of type string, but instead of type %s '%s'" % (type(value),repr(value)))
+
+def conver_float(value):
+    if value is None:
+        return None
+    elif isinstance(value,str):
+        try:
+            #locale.setlocale( locale.LC_ALL, 'en_US.UTF-8') 
+            return float(value)
+        except Exception as e:
+            logging.warning("Can't covert %s to float" % value,e)
+    else:
+        raise Exception("Conversion of none string to float is not supported '%s'" % repr(value))
+
+def conver_money(value):
+    if value is None:
+        return None
+    elif isinstance(value,str):
+        try:
+            #locale.setlocale( locale.LC_ALL, 'en_US.UTF-8') 
+            money_value = value.split(' ')[0]
+            if len(money_value) == 0:
+                return float(0)
+            else:
+                return float(money_value)
+        except Exception as e:
+            logging.warning("Can't covert %s to float/money" % value,e)
+    else:
+        raise Exception("Conversion of none string to float/money is not supported '%s'" % repr(value))
+
+def conver_datetime(value):
+    if value is None:
+        return None
+    elif isinstance(value,str):
+        try:
+            newvalue = str(value).split(' - ')[0]
+            return datetime.datetime.strptime(newvalue, "%Y-%m-%d %H:%M:%S")
+        except:
+            return datetime.datetime.strptime(value, "%Y-%m-%d - %H:%M")
+    else:
+        raise Exception("Conversion of none string to datetime is not supported '%s'" % repr(value))
+
+def conver_date(value):
+    if value is None:
+        return None
+    elif isinstance(value,str):
+        # Example: 2017-07-18
+        try:
+            value = str(value).split(' ')[0]
+            return datetime.datetime.strptime(value, "%Y-%m-%d")
+        except Exception as e:
+            logging.warning(e)
+    else:
+        raise Exception("Conversion of none string to datetime is not supported '%s'" % repr(value))
+
+def conver_dialog(value):
+    return conver_integer(value)
+
+def conver_gps(value):
+    if value is None:
+        return None
+    elif isinstance(value,dict):
+        latitude = value['latitude']
+        longitude = value['longitude']
+        return {'latitude': self.latitude, 'longitude': self.longitude }
+    else:
+        raise Exception("Value is not of type dict, but instead of type %s '%s'" % (type(value),repr(value)))
--- cmdb_idoit/category/value_factory.py
+++ cmdb_idoit/category/value_factory.py
@@ -15,18 +15,20 @@
     along with cmdb_idoit.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-
-from .value_base import *
-from .value_dialog import *
-from .value_text import *
-from .value_datetime import *
-from .value_gps import *
+from .conversion import *
 
 import pkg_resources
 import re
 import jsonpath_ng
+import datetime
 
-def read_rules():
+rules = None
+
+def _get_rules():
+    global rules
+    if rules != None:
+        return rules
+
     resource_package = __name__  # Could be any module/package name
     resource_path = '/'.join(('map', '1_9_4.map'))  # Do not use os.path.join(), see below
 
@@ -38,16 +40,82 @@ def read_rules():
             raise Exception("None conformal rule: %s" % line)
         if rule_raw[0] not in rules:
             rules[rule_raw[0]] = dict()
-        rules[rule_raw[0]][rule_raw[1]] = { 'type': rule_raw[2], 'path': rule_raw[3] }
+        jpath = jsonpath_ng.parse(rule_raw[3]) 
+        rules[rule_raw[0]][rule_raw[1]] = { 
+                'type': rule_raw[2], 
+                'path': rule_raw[3],
+                'jpath': jpath
+                }
     return rules
 
-rules = None
+def type_determination(category,key):
+
+    rules = _get_rules()
+    category_const = category.get_const()
+    # Do we have an overwrite for this category,key combination
+    if category_const in rules:
+        if key in rules[category_const]:
+            type_string = rules[category_const][key]['type']
+            if type_string == 'int':
+                return int
+            elif type_string == 'list_int':
+                return (list,int)
+            elif type_string == 'text':
+                return str 
+            elif type_string == 'list_text':
+                return (list,str)
+            elif type_string == 'double':
+                return float
+            elif type_string == 'list_double':
+                return (list,float)
+
+    # otherwise guess the type
+    data_type = category.getFieldObject(key)['data']['type']
+    info_type = category.getFieldObject(key)['info']['type']
+
+    base_type = None
+    if data_type == 'int':
+        base_type = int
+    elif data_type == 'float':
+        base_type = float
+    elif data_type == 'double':
+        base_type = float
+    elif data_type == 'text':
+        base_type = str
+    elif data_type == 'text_area':
+        base_type = str
+    elif data_type == 'date':
+        base_type = datetime.datetime
+    elif data_type == 'date_time':
+        base_type = datetime.datetime
+    else:
+        raise Exception('Unknown data_type %s' % data_type)
+
+    if info_type == 'dialog_list':
+        return (list,base_type)
+    elif info_type == 'object_browser':
+        return (list,base_type)
+    elif info_type == 'multiselect':
+        return (list,basetype)
+
+    return base_type
+
+def type_check(type_desc,value):
+    if isinstance(type_desc,tuple):
+        if not isinstance(value,type_desc[0]):
+            raise Exception("type check error '%s' is not a %s" % (value,type(type_desc[0])))
+        else:
+            for element in value:
+                if not isinstance(element,type_desc[1]):
+                    raise Exception("type check error the inner elements of '%s' are not of type %s" % (value,type(type_desc[1])))
+    elif not isinstance(value,type_desc):
+        raise Exception("type check error '%s' are not of type %s" % (value,type(type_desc)))
+    return True
+
 
 def value_representation_factory(category,key,value = None):
-    global rules
+    rules = _get_rules()
     category_const = category.get_const()
-    if rules is None:
-      rules = read_rules()
     data_type = category.getFieldObject(key)['data']['type']
     info_type = category.getFieldObject(key)['info']['type']
     if value == False:
@@ -60,98 +128,71 @@ def value_representation_factory(category,key,value = None):
         if category_const in rules:
             if key in rules[category_const]:
                 itype = rules[category_const][key]['type']
-                jpath = jsonpath_ng.parse(rules[category_const][key]['path'])
+                jpath = rules[category_const][key]['jpath']
                 matches = jpath.find(value)
                 if len(matches) == 0:
-                    raise Exception("Error matching value:",str(value))
+                    raise Exception("Error matching value,",rules[category_const][key]['path'],str(value))
                 if itype == 'int':
                     # Dialog Handling?
                     if len(matches) == 1:
-                      return CMDBCategoryValueInt(matches.pop().value)
+                      return conver_integer(matches.pop().value)
                     raise Exception("Expected one element but got multiple:",str(value))
                 elif itype == 'list_int':
-                    return CMDBCategoryValueListInt([ match.value for match in matches])
+                    return conver_list(conver_integer,[match.value for match in matches])
                 elif itype == 'double':
                     if len(matches) == 1:
-                        return CMDBCategoryValueDouble(matches.pop().value)
+                        return conver_float(matches.pop().value)
                 elif itype == 'gps':
                     if len(matches) == 1:
-                      return CMDBCategoryValueGPS(matches.pop().value)
+                      return conver_gps(matches.pop().value)
                     raise Exception("Expected one element but got multiple:",str(value))
                 elif itype == 'text':
                     if len(matches) == 1:
-                      return CMDBCategoryValueText(matches.pop().value)
+                      return conver_string(matches.pop().value)
                     raise Exception("Expected one element but got multiple:",str(value))
                 elif itype == 'list_text':
                     if len(matches) >= 1:
-                        return [CMDBCategoryValueText(match.value) for match in matches]
+                        return conver_list(conver_string,[match.value for match in matches])
                     raise Exception("Expected one element but got multiple:",str(value))
                 elif itype == 'money':
                     if len(matches) == 1:
-                      return CMDBCategoryValueMoney(matches.pop().value)
+                      return conver_money(matches.pop().value)
                     raise exception("Expected one element but got multiple:",str(value))
                 elif itype == 'dialog':
                     if len(matches) == 1:
-                      return CMDBCategoryValueDialog(matches.pop().value)
+                      return conver_dialog(matches.pop().value)
                     raise exception("Expected one element but got multiple:",str(value))
     return value_representation_factory_by_data_info(category.getFieldObject(key),value)
 
 def value_representation_factory_by_data_info(field_object, value=None):
     data_type = field_object['data']['type']
     info_type = field_object['info']['type']
-    try:
-        if data_type == 'int':
-            if info_type == 'dialog':
-                return CMDBCategoryValueDialog(value)
-            elif info_type == 'dialog_list':
-                # List?
-                return CMDBCategoryValueDialog(value)
-            elif info_type == 'dialog_plus':
-                return CMDBCategoryValueDialog(value)
-            elif info_type == 'object_browser':
-                return CMDBCategoryValueListInt(value)
-            elif info_type == 'int':
-                return CMDBCategoryValueInt(value)
-            elif info_type == 'n2m':
-                return CMDBCategoryValueBase(value)
-            elif info_type == 'multiselect':
-                return CMDBCategoryValueBase(value)
-            else:
-                raise NotImplementedError('Info type %s with data type %s is not implemented' % (info_type, data_type))
-        elif data_type == 'float':
-            if info_type == 'float':
-                return CMDBCategoryValueBase(value)
-            else:
-                raise NotImplementedError('Info type %s with data type %s is not implemented' % (info_type, data_type))
-        elif data_type == 'double':
-            if info_type == 'double':
-                return CMDBCategoryValueDouble(value)
-            elif info_type == 'money':
-                return CMDBCategoryValueMoney(value)
-            else:
-                raise NotImplementedError('Info type %s with data type %s is not implemented' % (info_type, data_type))
-        elif data_type == 'text_area':
-            return CMDBCategoryValueBase(value)
-        elif data_type == 'text':
-            if info_type == 'text':
-                return CMDBCategoryValueText(value)
-            elif info_type == 'multiselect':
-                return CMDBCategoryValueBase(value)
-            elif info_type == 'dialog':
-                return CMDBCategoryValueDialog(value)
-            elif info_type == 'dialog_list':
-                # List?
-                return CMDBCategoryValueDialog(value)
-            elif info_type == 'upload':
-                return CMDBCategoryValueBase(value)
-            else:
-                raise NotImplementedError('Info type %s with data type %s is not implemented' % (info_type, data_type))
-        elif data_type == 'date_time':
-            return CMDBCategoryValueDateTime(value)
-        elif data_type == 'date':
-            if info_type == 'date':
-                return CMDBCategoryValueDate(value)
+
+    if data_type == 'int':
+        conver_func = conver_integer
+    elif data_type == 'float':
+        conver_func = conver_float 
+    elif data_type == 'double':
+        if info_type == 'money':
+            conver_func = conver_money
         else:
-            raise NotImplementedError('Data type %s is not implement' % data_type)
-    except Exception as e:
-        raise Exception('Error with value of data type %s and info type %s' % (data_type, info_type), e, value)
+            conver_func = conver_float
+    elif data_type == 'text':
+        conver_func = conver_string
+    elif data_type == 'text_area':
+        conver_func = conver_string
+    elif data_type == 'date':
+        conver_func = conver_date
+    elif data_type == 'date_time':
+        conver_func = conver_datetime
+    else:
+        raise Exception('Unknown data_type %s',data_type)
+
+    if info_type == 'dialog_list':
+        return conver_list(conver_func,value)
+    elif info_type == 'object_browser':
+        return conver_list(conver_func,value)
+    elif info_type == 'multiselect':
+        return conver_list(conver_func,value)
+    else:
+        return conver_func(value)
